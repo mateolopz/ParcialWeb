72c73a1fc5024ec1c4c9c4286978348109a83dbb
Mateo Lopez <m.lopez23@uniandes.edu.co>
2024-09-26 09:55:29 -0500

    login listo

---
+++ parcial1/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Login from './components/Login.js'
+import Container from 'react-bootstrap/Container';
+
+function App() {
+  return (
+  <Container fluid>
+    <Login/>
+  </Container>
+  )
+}
+
+export default App;
---
+++ parcial1/src/components/Login.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState }from 'react';
+import Container from 'react-bootstrap/Container';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import '../App.css';
+
+const Login = () =>{
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    
+    function validateEmail(email) {
+        var re = /\S+@\S+\.\S+/;
+        return re.test(email);
+      }
+
+    const send_alert = () => {
+        if (password.length != 8){
+            alert("La contraseña debe tener 8 caracteres")
+        } else if (validateEmail(email) == false){
+            alert("Correo invalido")
+        }
+    }
+
+    return(
+        <Container fluid className="login">
+            <Col className="login" style={{marginTop:"15%"}}>
+                <text style={{alignContent:"left"}}><strong>Log in</strong></text>
+                <text>Email</text>
+                <input onChange= {(event)=> setEmail(event.target.value)}></input>
+                <text>Password</text>
+                <input onChange= {(event)=> setPassword(event.target.value)}></input>
+                <input type="submit" value="Log in" onClick={()=>send_alert()}></input>
+            </Col>
+        </Container>
+    )
+}
+
+export default Login;
